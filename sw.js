e67ae28a7f5b6b1ef6b4861c03c86ee9bf276885
Hansraj <hans182007@gmail.com>
2022-05-11 10:12:12 +0530

    .

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./","./home.css","s.png"]);
+            return cache.addAll(["./","./home.css","s.png","home.js","Ubuntu-Title.ttf","sw1.png"]);
         })
     );
 });
